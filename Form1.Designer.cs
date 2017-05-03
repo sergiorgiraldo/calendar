b427aea17700a5b2a191798e47ca7513e561990d
Sergio Giraldo <sgiraldo@uolinc.com>
2017-05-03 11:06:24 -0300

    20170503

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,34 +33,38 @@
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(3, 2);
-            this.monthCalendar1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.monthCalendar1.Location = new System.Drawing.Point(8, 6);
+            this.monthCalendar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(3, 3);
+            this.monthCalendar1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthCalendar1.Location = new System.Drawing.Point(23, 20);
             this.monthCalendar1.Margin = new System.Windows.Forms.Padding(14, 16, 14, 16);
             this.monthCalendar1.MaxSelectionCount = 1;
             this.monthCalendar1.MonthlyBoldedDates = new System.DateTime[] {
         new System.DateTime(2014, 10, 1, 0, 0, 0, 0)};
             this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
             this.monthCalendar1.TabIndex = 0;
             this.monthCalendar1.TitleBackColor = System.Drawing.Color.Gainsboro;
             this.monthCalendar1.TitleForeColor = System.Drawing.Color.Black;
             this.monthCalendar1.TrailingForeColor = System.Drawing.Color.Red;
             this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            this.monthCalendar1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.monthCalendar1_KeyDown);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 480);
+            this.ClientSize = new System.Drawing.Size(773, 683);
             this.Controls.Add(this.monthCalendar1);
             this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
             this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calendar";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
--- Form1.cs
+++ Form1.cs
@@ -23,13 +23,32 @@ namespace Calendar
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
             if (e.KeyCode == Keys.P || e.KeyCode == Keys.J)
                 monthCalendar1.SetDate(monthCalendar1.GetDisplayRange(true).Start.AddMonths(-1));
             else if (e.KeyCode == Keys.N || e.KeyCode == Keys.K)
                 monthCalendar1.SetDate(monthCalendar1.GetDisplayRange(true).End.AddMonths(1));
             else if (e.KeyCode == Keys.T || e.KeyCode == Keys.H)
                 monthCalendar1.SetDate(DateTime.Now.Date);
+            else if (e.KeyCode == Keys.X)
+                Close();
+            else if (e.KeyCode == Keys.F1)
+            {
+                MessageBox.Show(@"
+KeyCode == Keys.P || Keys.J)\n
+    SetDate AddMonths(-1))\n
+KeyCode == Keys.N || Keys.K)\n
+	SetDate AddMonths(1))\n
+KeyCode == Keys.T || Keys.H)\n
+	SetDate DateTime.Now.Date\n
+KeyCode == Keys.X)\n
+	Close()\n
+");
+            }
+            //else if (e.KeyCode == Keys.Right)
+            //    monthCalendar1.SetDate(monthCalendar1.SelectionRange.Start.AddDays(1));
+            //else if (e.KeyCode == Keys.Left)
+            //    monthCalendar1.SetDate(monthCalendar1.SelectionRange.Start.AddDays(-1));
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
@@ -39,6 +58,16 @@ namespace Calendar
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
 
+        }
+
+        private void monthCalendar1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_HelpButtonClicked(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
         }
     }
 }
